97ca91f2a3478eadc9fa267bc8c384343022dd2f
Maurine Basaia <mbasaia@pubfac.com>
2026-06-19 12:12:41 +0200

    vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from "react-router";
 import { useNavigate, useLocation } from "react-router";
+import { Analytics } from "@vercel/analytics/next";
 
 import Platforms from "./components/Platforms";
 import Category from "./components/Category";
@@ -9,13 +10,16 @@ import Results from "./components/Result";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Platforms />} />
-      <Route path="/preference" element={<Preference />} />
-      <Route path="/category" element={<Category />} />
-      <Route path="/search" element={<Search />} />
-      <Route path="/results" element={<Results />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<Platforms />} />
+        <Route path="/preference" element={<Preference />} />
+        <Route path="/category" element={<Category />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/results" element={<Results />} />
+      </Routes>
+      <Analytics />
+    </>
   );
 }
 
