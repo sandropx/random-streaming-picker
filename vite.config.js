dabe45292ceee55d0f2822fd95c66b4475705664
Maurine Basaia <mbasaia@pubfac.com>
2026-07-01 18:36:06 +0200

    bug cache

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,11 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: "autoUpdate",
+      workbox: {
+        cleanupOutdatedCaches: true,
+        skipWaiting: true,
+        clientsClaim: true,
+      },
       manifest: {
         id: "/",
         name: "Random Streaming Picker",
